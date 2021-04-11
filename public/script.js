f0ce772694b48fbc0beb46d9e1e716b138b7f594
jmcint21 <50252036+jmcint21@users.noreply.github.com>
2021-04-10 20:18:44 -0400

    Added Random generator

--- public/script.js
+++ public/script.js
@@ -1,3 +1,9 @@
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+  }
+
 async function getMeals() {
     console.log('data request');
     const diningRequest = await fetch('/api/wholeMeal');
@@ -7,8 +13,16 @@ async function getMeals() {
 
 async function windowActions() {
     console.log('loaded window');
-    const meals = await getMeals();
-    console.table(meals.data);
+    const results = await getMeals();
+    const meals = results.data;
+    
+    const mealArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const selectedMeals = mealArray.map((element) => {
+        const random = getRandomIntInclusive(0, meals.length -1);
+        return meals[random];
+    });
+
+    console.table(selectedMeals);
 }
 
 window.onload = windowActions;
